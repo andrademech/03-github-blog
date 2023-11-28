c59e3a9239034a1e2d9e43425d2375dacc4fad10
Betim <dev.herberth@gmail.com>
2023-11-27 22:15:42 -0300

    develop: starting develop branch

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      blue: '#3294F8',
+      'base-title': '#E7EDF4',
+      'base-subtitle': '#C4D4E3',
+      'base-text': '#AFC2D4',
+      'base-span': '#7B96B2',
+      'base-label': '#3A536B',
+      'base-border': '#1C2F41',
+      'base-post': '#112131',
+      'base-profile': '#0B1B2B',
+      'base-background': '#071422',
+      'base-input': '#040F1A',
     },
   },
   plugins: [],
